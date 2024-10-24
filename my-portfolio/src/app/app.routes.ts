45c2cbb3db40b114e8f8982626b2b6b13fdd1ce5
adityajaiswal37 <adijaiswal0307@gmail.com>
2024-10-24 11:18:27 +0530

    fix

--- my-portfolio/src/app/app.routes.ts
+++ my-portfolio/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+const mainComponentChildren: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: MainComponent, children:  mainComponentChildren }
+  ];
