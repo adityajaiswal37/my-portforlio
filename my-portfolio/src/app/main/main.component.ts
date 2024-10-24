e4549ec34b9d1be3b49ae170804fc79488d42255
adityajaiswal37 <adijaiswal0307@gmail.com>
2024-10-24 12:46:28 +0530

    new components added

--- my-portfolio/src/app/main/main.component.ts
+++ my-portfolio/src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
+import { HomeComponent } from '../components/home/home.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
